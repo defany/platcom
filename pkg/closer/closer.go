4835cf2b8a97eae240fbd5bff7c2d4b11a6186d1
defany <dimamileris2006@gmail.com>
2025-04-22 16:47:29 +0200

    refactor closer

--- pkg/closer/closer.go
+++ pkg/closer/closer.go
@@ -1,62 +1,72 @@
 package closer
 
 import (
+	"context"
 	"errors"
 	"log/slog"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 )
 
-var closer = New(os.Interrupt, syscall.SIGTERM)
+var defaultCloser = NewWithLogger(slog.Default(), os.Interrupt, syscall.SIGTERM)
+
+func SetLogger(logger *slog.Logger) {
+	defaultCloser.SetLogger(logger)
+}
 
 func Add(fns ...func() error) {
-	closer.Add(fns...)
+	defaultCloser.Add(fns...)
 }
 
 func Close() {
-	closer.Close()
-
-	closer.Wait()
+	defaultCloser.Close()
+	defaultCloser.Wait()
 }
 
 type Closer struct {
-	mu   sync.Mutex
-	once sync.Once
-	done chan struct{}
-	fns  []func() error
+	mu     sync.Mutex
+	once   sync.Once
+	done   chan struct{}
+	fns    []func() error
+	logger *slog.Logger
 }
 
 func New(sig ...os.Signal) *Closer {
+	return NewWithLogger(slog.Default(), sig...)
+}
+
+func NewWithLogger(logger *slog.Logger, sig ...os.Signal) *Closer {
 	c := &Closer{
-		done: make(chan struct{}),
+		done:   make(chan struct{}),
+		logger: logger,
 	}
 
-	if len(sig) == 0 {
-		return c
+	if len(sig) > 0 {
+		go func() {
+			ch := make(chan os.Signal, 1)
+			signal.Notify(ch, sig...)
+			<-ch
+			signal.Stop(ch)
+			c.Close()
+		}()
 	}
-
-	go func() {
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, sig...)
-		<-ch
-
-		signal.Stop(ch)
-
-		c.Close()
-	}()
 
 	return c
+}
+
+func (c *Closer) SetLogger(logger *slog.Logger) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.logger = logger
 }
 
 func (c *Closer) Add(fns ...func() error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-
 	c.fns = append(c.fns, fns...)
-
-	return
 }
 
 func (c *Closer) Wait() {
@@ -69,23 +79,35 @@ func (c *Closer) Close() {
 
 		c.mu.Lock()
 		fns := c.fns
-
+		logger := c.logger
 		c.fns = nil
 		c.mu.Unlock()
 
-		var err error
+		var errs []error
 
-		for _, fn := range fns {
-			// TODO: if close too long - kill
-			if ferr := fn(); err != nil {
-				err = errors.Join(err, ferr)
+		for i, fn := range fns {
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			done := make(chan error, 1)
+			go func() {
+				done <- fn()
+			}()
+
+			select {
+			case <-ctx.Done():
+				logger.Error("closer timeout", slog.Int("fn_index", i), slog.String("error", ctx.Err().Error()))
+				errs = append(errs, ctx.Err())
+			case ferr := <-done:
+				if ferr != nil {
+					logger.Error("closer function failed", slog.Int("fn_index", i), slog.String("error", ferr.Error()))
+					errs = append(errs, ferr)
+				}
 			}
 		}
 
-		if err != nil {
-			slog.Error("failed to close all functions", slog.String("error", err.Error()))
+		if len(errs) > 0 {
+			logger.Error("failed to close all functions", slog.String("error", errors.Join(errs...).Error()))
 		}
-
-		return
 	})
 }
